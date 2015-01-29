e16f2b2fb1df1650dec8e2cb08c513892a9cf20a
oskarh <oskar.hojwall@schibstedpayment.no>
2015-01-29 10:38:21 +0100

    Changed to use /email/{email}/status endpoint instead of /endpoints as clients don't have access to it by default

--- getting-started/src/main/java/no/spid/examples/GettingStarted.java
+++ getting-started/src/main/java/no/spid/examples/GettingStarted.java
@@ -5,26 +5,30 @@ import no.spid.api.exceptions.SpidApiException;
 import no.spid.api.exceptions.SpidOAuthException;
 import no.spid.api.oauth.SpidOAuthToken;
 
+import org.apache.commons.codec.binary.Base64;
+
 public class GettingStarted {
 
     public static void main(String[] args) {
-        String clientId = args[0];
-        String secret = args[1];
-        String signatureSecret = "";
-        String redirectUrl = "http://localhost:8080";
-        String spidBaseUrl = "https://stage.payment.schibsted.no";
+        final String clientId = args[0];
+        final String secret = args[1];
+        final String signatureSecret = "";
+        final String redirectUrl = "http://localhost:8080";
+        final String spidBaseUrl = "https://stage.payment.schibsted.no";
+
+        SpidApiClient client = new SpidApiClient.ClientBuilder(
+                clientId,
+                secret,
+                signatureSecret,
+                redirectUrl,
+                spidBaseUrl
+        ).build();
 
         try {
-            SpidApiClient client = new SpidApiClient.ClientBuilder(
-                    clientId,
-                    secret,
-                    signatureSecret,
-                    redirectUrl,
-                    spidBaseUrl
-            ).build();
-
-            SpidOAuthToken token = client.getServerToken();
-            String responseJSON = client.GET(token, "/endpoints", null).getRawData();
+         SpidOAuthToken token = client.getServerToken();
+            final String email = "some.email@google.com";   // check to see if this email exists
+            String base64EncodedEmail = new String(Base64.encodeBase64(email.getBytes()));
+            String responseJSON = client.GET(token, "/email/" + base64EncodedEmail + "/status", null).getRawData();
             System.out.println(responseJSON);
         } catch (SpidOAuthException e) {
             e.printStackTrace();
